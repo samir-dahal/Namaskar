763909cf8fc25675e3c8bbc73db3a33a7561cc7f
Samir Dahal <samirdahal175@gmail.com>
2026-05-31 15:47:56 +0545

    Add project files.

---
+++ Namaskar.Extension/BackgroundWorker.cs
@@ -0,0 +1,20 @@
+﻿using Blazor.BrowserExtension;
+
+namespace Namaskar.Extension
+{
+    public partial class BackgroundWorker : BackgroundWorkerBase
+    {
+        [BackgroundWorkerMain]
+        public override void Main()
+            => WebExtensions.Runtime.OnInstalled.AddListener(OnInstalled);
+
+        async Task OnInstalled()
+        {
+            var indexPageUrl = WebExtensions.Runtime.GetURL("index.html");
+            await WebExtensions.Tabs.Create(new()
+            {
+                Url = indexPageUrl
+            });
+        }
+    }
+}
---
+++ Namaskar.Extension/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazor.BrowserExtension;
+using Namaskar.Extension;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.UseBrowserExtension(browserExtension =>
+{
+    if (browserExtension.Mode == BrowserExtensionMode.Background)
+    {
+        builder.RootComponents.AddBackgroundWorker<BackgroundWorker>();
+    }
+    else
+    {
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+    }
+});
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
